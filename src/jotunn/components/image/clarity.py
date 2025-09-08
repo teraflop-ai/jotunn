a7135b2a74099ec50d87061e8b145b2dfbbae3b1
Enrico Shippole <enricoship@gmail.com>
2025-09-07 23:25:29 -0400

    Clarity filter and example

---
+++ src/jotunn/components/image/clarity.py
@@ -0,0 +1,33 @@
+from typing import Optional
+
+import cv2
+import daft
+import numpy as np
+from daft import DataType
+
+from jotunn.components.base import ScoreFilter
+
+
+class Clarity(ScoreFilter):
+    def __init__(
+        self,
+        input_column: str = None,
+        output_column: Optional[str] = "clarity_score",
+        daft_dtype: DataType = DataType.float32(),
+        threshold: Optional[float] = None,
+    ):
+        super().__init__(
+            input_column=input_column,
+            output_column=output_column,
+            daft_dtype=daft_dtype,
+            threshold=threshold,
+        )
+
+    def _score(self, image: np.array) -> float:
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        clarity_score = cv2.Laplacian(gray, cv2.CV_32F).var() * gray.std()
+        return float(clarity_score)
+
+    def _filter(self, df: daft.DataFrame, threshold: float) -> daft.DataFrame:
+        df = df.where(df[self.output_column] >= threshold)
+        return df
