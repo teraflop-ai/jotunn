847d766c84a57b18c83c75e062b0eb99827fa914
Enrico Shippole <enricoship@gmail.com>
2025-09-08 22:18:32 -0400

    Saturation filter and example

--- examples/image/saturation_score.py
+++ examples/image/saturation_score.py
@@ -1,22 +1,23 @@
 import daft
 from daft import col
 
-from jotunn.components.image.contrast import Contrast
+from jotunn.components.image.saturation import Saturation
 
 df = daft.from_pydict(
     {
         "urls": [
-            "https://live.staticflickr.com/65535/53671838774_03ba68d203_o.jpg",
+            "https://live.staticflickr.com/2498/4065367744_54865768dc_b.jpg",
             "https://live.staticflickr.com/65535/53671700073_2c9441422e_o.jpg",
             "https://live.staticflickr.com/65535/53670606332_1ea5f2ce68_o.jpg",
             "https://live.staticflickr.com/65535/53671838039_b97411a441_o.jpg",
             "https://live.staticflickr.com/65535/53671698613_0230f8af3c_o.jpg",
             "https://cdn.visualwilderness.com/wp-content/uploads/2022/12/new_mexico_9791.jpg",
+            "https://f64academy.com/~f64academy/wp-content/uploads/2012/11/Before-512x385.jpg",
         ],
     }
 )
 
-contrast_filter = Contrast(input_column="image", contrast=0.35)
+contrast_filter = Saturation(input_column="image", threshold=0.6)
 
 df = df.with_column("image_bytes", col("urls").url.download(on_error="null"))
 df = df.with_column("image", col("image_bytes").image.decode())
---
+++ src/jotunn/components/image/saturation.py
@@ -0,0 +1,37 @@
+from typing import Optional
+
+import cv2
+import daft
+import numpy as np
+from daft import DataType
+
+from jotunn.components.base import ScoreFilter
+
+
+class Saturation(ScoreFilter):
+    def __init__(
+        self,
+        input_column: str = None,
+        output_column: Optional[str] = "saturation_score",
+        daft_dtype: DataType = DataType.float32(),
+        threshold: Optional[float] = None,
+    ):
+        super().__init__(
+            input_column=input_column,
+            output_column=output_column,
+            daft_dtype=daft_dtype,
+            threshold=threshold,
+        )
+
+    def _score(self, image: np.array) -> float:
+        if image is None:
+            return -1
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        saturation = cv2.mean(gray)[1] / 255.0
+        return saturation
+
+    def _filter(self, df: daft.DataFrame, threshold: float) -> daft.DataFrame:
+        df = df.where(
+            (df[self.output_column] <= threshold) | (df[self.output_column] == -1)
+        )
+        return df
