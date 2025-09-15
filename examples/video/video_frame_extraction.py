38a871d09cb31e7a7021ff71448f5160134328fe
Enrico Shippole <enricoship@gmail.com>
2025-09-15 17:00:49 -0400

    Video frame extractor and example

--- examples/video/video_frame_extraction.py
+++ examples/video/video_frame_extraction.py
@@ -1,24 +1,36 @@
+import os
+
 import cv2
 import daft
 import numpy as np
-from daft import DataType, col
 
-from jotunn.components.video.frame_extractor import extract_random_frame_sample
+from jotunn.components.image.resize import Resize
+from jotunn.components.video.frame_extractor import FrameExtractor
 
-df = daft.from_pydict({"filepath": ["/video/my.mp4"]})
-df = df.with_column(
-    "image_bytes",
-    col("filepath").apply(
-        extract_random_frame_sample, return_dtype=DataType.list(dtype=DataType.binary())
-    ),
+daft.set_execution_config(parquet_target_filesize=5 * 1024 * 1024 * 1024)
+
+df = daft.from_pydict(
+    {
+        "filepath": [
+            "/Parallel Computer Architecture and Programming, Lecture 4 (Tsinghua⧸CMU 2017 Summer Course) [yNP6RsiZNXQ].mkv"
+        ]
+    }
 )
-df = df.explode("image_bytes")
-df = df.with_column("image", col("image_bytes").image.decode())
-df = df.with_column("image", df["image"].image.resize(256, 256))
-df.write_parquet("output_frames")
 
+extractor = FrameExtractor(input_column="filepath", number_frames=8, encoding=".webp")
+resized = Resize(input_column="image", output_column="image")
+
+df = extractor(df)
+df = resized(df)
+
+os.makedirs("/output_frames", exist_ok=True)
+df.write_parquet("/output_frames")
+
+df.show()
+
+os.makedirs("/extracted_frames", exist_ok=True)
 for i, img in enumerate(df.to_pydict()["image"]):
     cv2.imwrite(
-        f"extracted_frames/frame_{i}.webp",
+        f"/extracted_frames/frame_{i}.webp",
         cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR),
     )
--- src/jotunn/components/video/frame_extractor.py
+++ src/jotunn/components/video/frame_extractor.py
@@ -2,30 +2,55 @@ from typing import List
 
 import cv2
 import numpy as np
+from daft import DataType, col
 from video_reader import PyVideoReader
 
 
-def extract_random_frame_sample(filepath: str) -> List:
-    try:
-        vr = PyVideoReader(filepath)
-        info_dict = vr.get_info()
-        total_frames = int(info_dict["frame_count"])
+class FrameExtractor:
+    def __init__(
+        self,
+        input_column: str,
+        output_column: str = "image",
+        number_frames: int = 8,
+        encoding: str = ".webp",
+    ):
+        self.input_column = input_column
+        self.output_column = output_column
+        self.number_frames = number_frames
+        self.encoding = encoding
 
-        if total_frames == 0:
+    def _extract(self, filepath: str) -> List:
+        try:
+            vr = PyVideoReader(filepath)
+            info_dict = vr.get_info()
+            total_frames = int(info_dict["frame_count"])
+
+            if total_frames == 0:
+                return []
+
+            num_to_sample = min(total_frames, self.number_frames)
+            indices = np.random.choice(total_frames, size=num_to_sample, replace=False)
+            indices.sort()
+            frames = vr.get_batch(indices.tolist())
+
+            images = []
+            for frame in frames:
+                _, encoded_image = cv2.imencode(self.encoding, frame)
+                image = encoded_image.tobytes()
+                images.append(image)
+            return images
+
+        except Exception as e:
+            print(f"Error processing {filepath}: {e}")
             return []
 
-        num_to_sample = min(total_frames, 4)
-        indices = np.random.choice(total_frames, size=num_to_sample, replace=False)
-        indices.sort()
-        frames = vr.get_batch(indices.tolist())
-
-        images = []
-        for frame in frames:
-            _, encoded_image = cv2.imencode(".webp", frame)
-            image = encoded_image.tobytes()
-            images.append(image)
-        return images
-
-    except Exception as e:
-        print(f"Error processing {filepath}: {e}")
-        return []
+    def __call__(self, df):
+        df = df.with_column(
+            "image_bytes",
+            col(self.input_column).apply(
+                self._extract, return_dtype=DataType.list(dtype=DataType.binary())
+            ),
+        )
+        df = df.explode("image_bytes")
+        df = df.with_column(self.output_column, col("image_bytes").image.decode())
+        return df
