f13b6cdcf31bbba37d7eb7a6160c18230ec71e1e
Enrico Shippole <enricoship@gmail.com>
2025-09-15 00:32:52 -0400

    Add upper and lower bounds to thresholds

--- src/jotunn/components/base.py
+++ src/jotunn/components/base.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Any, Optional
+from typing import Any, Optional, Union
 
 import daft
 from daft import DataType, col
@@ -13,22 +13,27 @@ class ScoreFilter(ABC):
         input_column: str,
         output_column: Optional[str] = None,
         daft_dtype: DataType = None,
-        threshold: Optional[float] = None,
+        min_threshold: Optional[Union[float, int]] = None,
+        max_threshold: Optional[Union[float, int]] = None,
     ):
         self.input_column = input_column
         self.output_column = output_column
         self.daft_dtype = daft_dtype
-        self.threshold = threshold
+        self.min_threshold = min_threshold
+        self.max_threshold = max_threshold
 
     @abstractmethod
     def _score(self) -> Any:
         """Create and return the score."""
         pass
 
-    @abstractmethod
-    def _filter(self) -> Any:
+    def _filter(self, df: daft.DataFrame) -> daft.DataFrame:
         """Fitler the Dataframe depending on the score."""
-        pass
+        if self.min_threshold is not None:
+            df = df.where(df[self.output_column] >= self.min_threshold)
+        if self.max_threshold is not None:
+            df = df.where(df[self.output_column] <= self.max_threshold)
+        return df
 
     def __call__(self, df: daft.DataFrame) -> daft.DataFrame:
         """Apply the score and filter to the dataframe."""
@@ -38,6 +43,6 @@ class ScoreFilter(ABC):
                 lambda x: self._score(x), return_dtype=self.daft_dtype
             ),
         )
-        if self.threshold:
-            df = self._filter(df, self.threshold)
+        if self.min_threshold is not None or self.max_threshold is not None:
+            df = self._filter(df)
         return df
